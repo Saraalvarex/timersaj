f724a4965102162b03a67b268a982ad569b5c408
saraalvarex <alvarezalmacen19@gmail.com>
2023-01-02 22:34:49 +0100

    temporizador

--- src/components/Temporizador.js
+++ src/components/Temporizador.js
@@ -16,11 +16,25 @@ export default class Temporizador extends Component {
     var url = Global.timer + request;
 
     axios.get(url).then(res => {
-        console.log(res)
+        var timers = res.data;
+        var sala = "";
+        var tiempoTotal = 0;
+        var categorias = []
+        console.log(timers)
+        timers.forEach(timer => {
+            sala = timer.sala;
+            categorias.push(timer.categoria);
+            console.log(timer.inicioEvento);
+            const inicio = new Date(timer.inicioEvento);
+            const fin = new Date(timer.finEvento);
+            tiempoTotal = (fin.getTime() - inicio.getTime()) / 3600000; // 5
+        });
+
       this.setState({
-        timers: res,
-        status: true,
-        mensaje: "Get timer"
+        sala: sala,
+        categorias: categorias,
+        tiempoTotal: tiempoTotal,
+        status: true
       });
     });
   }
@@ -33,9 +47,9 @@ export default class Temporizador extends Component {
 
     return (
         <div>
-            <p>Sala {this.state.timers}</p>
+            <p>Sala {this.state.sala}</p>
 
-            <h1>CATEGORIAS DE TIEMPO</h1>
+            <h1>Tiempo total {this.state.tiempoTotal}</h1>
 
         </div>
     )
